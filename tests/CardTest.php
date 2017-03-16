abb43c8655c23e6168986e4d8c6c7f4f1a901e81
Daniel Hardy <sgdhardy1991@gmail.com>
2017-03-16 20:06:06 +0000

    Added new attributes and tests for card class. refactored test file

--- tests/CardTest.php
+++ tests/CardTest.php
@@ -10,7 +10,7 @@ class TestExample extends PHPUnit_Framework_TestCase
      */
     public function canGetACard()
     {
-        $card = new Card(2, 4, 3);
+        $card = new Card(2, 4, 3, 5, 8, 1);
         $this->assertInstanceOf(Card::class, $card);
     }
 
@@ -19,14 +19,20 @@ class TestExample extends PHPUnit_Framework_TestCase
      */
     public function cardHasStats()
     {
-        $weakCard = new Card(2, 4, 3);
+        $weakCard = new Card(2, 4, 3, 5, 8, 1);
         $strength = $weakCard->getStrength();
         $fearFactor = $weakCard->getFearFactor();
         $magic = $weakCard->getMagic();
+        $rage = $weakCard->getRage();
+        $alchemy = $weakCard->getAlchemy();
+        $stealth = $weakCard->getStealth();
 
         $this->assertEquals($strength, 2);
         $this->assertEquals($fearFactor, 4);
         $this->assertEquals($magic, 3);
+        $this->assertEquals($rage, 5);
+        $this->assertEquals($alchemy, 8);
+        $this->assertEquals($stealth, 1);
     }
 }
 
