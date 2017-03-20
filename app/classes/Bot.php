89c4c365e7a337d507ae57de1c8870f4e4f56810
Daniel Hardy <sgdhardy1991@gmail.com>
2017-03-20 21:59:22 +0000

    Added bot take turn and get card functionality with tests

--- app/classes/Bot.php
+++ app/classes/Bot.php
@@ -2,4 +2,36 @@
 
 class Bot
 {
+    public $cardDeck = [];
+
+    public function takeTurn()
+    {
+        $card = $this->getCard();
+        return $this->getHighestStat($card);
+    }
+
+    public function giveCard(Card $card)
+    {
+        $this->cardDeck[] = $card;
+    }
+
+    private function getCard()
+    {
+        return array_shift($this->cardDeck);
+    }
+
+    private function getHighestStat(Card $card)
+    {
+        $stats = [
+            "Alchemy" => $card->getAlchemy(),
+            "Fear Factor" => $card->getFearFactor(),
+            "Magic" => $card->getMagic(),
+            "Rage" => $card->getRage(),
+            "Stealth" => $card->getStealth(),
+            "Strength" =>$card->getStrength()
+        ];
+
+        arsort($stats);
+        return current(array_keys($stats));
+    }
 }
--- tests/BotTest.php
+++ tests/BotTest.php
@@ -4,13 +4,70 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 class BotTest extends PHPUnit_Framework_TestCase
 {
+    private $bot;
+
+    public function setUp()
+    {
+        $this->bot = new Bot();
+    }
+
     /**
      * @test
      */
     public function canCreateABot()
     {
-        $bot = new Bot();
-        $this->assertInstanceOf(Bot::class, $bot);
+        $this->assertInstanceOf(Bot::class, $this->bot);
+    }
+
+    /**
+     * @test
+     */
+    public function botReceivesACard()
+    {
+        $initialDeckSize = count($this->bot->cardDeck);
+        $this->bot->giveCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
+        $this->assertEquals($initialDeckSize + 1, count($this->bot->cardDeck));
+    }
+
+    /**
+     * @test
+     * @dataProvider cardProvider
+     */
+    public function choosesHighestValueStat($card, $expectedStat)
+    {
+        $this->bot->giveCard($card);
+        $this->assertEquals($expectedStat, $this->bot->takeTurn());
+    }
+
+    private function cardProvider()
+    {
+        return [
+            [
+                new Card("Lab rat", 10, 6, 5, 3, 5, 4),
+                "Strength"
+            ],
+            [
+                new Card("Voldermort", 9, 20, 15, 15, 12, 2),
+                "Fear Factor"
+            ],
+            [
+                new Card("Merlin", 10, 6, 14, 3, 5, 4),
+                "Magic"
+            ],
+            [
+                new Card("Hulk", 13, 6, 0, 20, 0, 0),
+                "Rage"
+            ],
+            [
+                new Card("Nicholas Flamel", 12, 12, 5, 3, 20, 4),
+                "Alchemy"
+            ],
+            [
+                new Card("Snake", 10, 15, 0, 12, 0, 16),
+                "Stealth"
+            ]
+        ];
     }
 }
+
 ?>
