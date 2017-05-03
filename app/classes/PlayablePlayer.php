cfe50daf36535c728d0f5efa1cc3ffd1f15f774c
Dan <sgdhardy1991@gmail.com>
2017-05-03 20:36:46 +0100

    Added carddeck and collectCard, getCard functionality

--- app/classes/PlayablePlayer.php
+++ app/classes/PlayablePlayer.php
@@ -2,5 +2,25 @@
 
 class PlayablePlayer
 {
+    public $cardDeck = [];
 
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function collectCard(Card $card)
+    {
+        $this->cardDeck[] = $card;
+    }
+
+    public function getCard()
+    {
+        return array_shift($this->cardDeck);
+    }
 }
--- tests/PlayablePlayerTest.php
+++ tests/PlayablePlayerTest.php
@@ -2,14 +2,58 @@
 
 class PlayablePlayerTest extends PHPUnit_Framework_TestCase
 {
+    protected $playablePlayer;
+
+    public function setUp()
+    {
+        $this->playablePlayer = new PlayablePlayer('Player 1');
+    }
+
     /**
      * @test
      */
-    public function canCreateAPlayablePlayer()
+    public function canBeCreated()
     {
-        $this->playablePlayer = new PlayablePlayer();
         $this->assertInstanceOf(PlayablePlayer::class, $this->playablePlayer);
     }
 
+    /**
+     * @test
+     */
+    public function canGetName()
+    {
+        $this->assertEquals('Player 1', $this->playablePlayer->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function canCollectACard()
+    {
+        $initialDeckSize = count($this->playablePlayer->cardDeck);
+        $this->playablePlayer->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
+        $this->assertEquals($initialDeckSize + 1, count($this->playablePlayer->cardDeck));
+    }
+
+    /**
+     * @test
+     */
+    public function canLoseACard()
+    {
+        $this->playablePlayer->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
+        $initialDeckSize = count($this->playablePlayer->cardDeck);
+        $this->playablePlayer->getCard();
+        $this->assertEquals($initialDeckSize - 1, count($this->playablePlayer->cardDeck));
+    }
+
+    /**
+     * @test
+     */
+    public function cardLostIsACard()
+    {
+        $this->playablePlayer->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
+        $card = $this->playablePlayer->getCard();
+        $this->assertInstanceOf(Card::class, $card);
+    }
 }
 ?>
