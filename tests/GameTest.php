a2210cd5e2ca0970742982e791365f4c4a04a578
Dan <sgdhardy1991@gmail.com>
2017-05-04 20:38:28 +0100

    Added tests back

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,47 +1,39 @@
 <?php
 
-//class GameTest extends PHPUnit_Framework_TestCase
-//{
-//
-//    public $game;
-//
-//    public function setUp()
-//    {
-//        ob_start();
-//        $this->game = new Game();
-//        ob_end_clean();
-//    }
-//
-//    /**
-//     * @test
-//     */
-//    public function canCreateAGame()
-//    {
-//        $this->assertInstanceOf(Game::class, $this->game);
-//    }
-//
-//    /**
-//     * @test
-//     */
-//    public function newGameHasTwoBots()
-//    {
-//        $this->assertInstanceOf(Bot::class, $this->game->botOne);
-//        $this->assertInstanceOf(Bot::class, $this->game->botTwo);
-//    }
-//
-//    /**
-//     * @test
-//     */
-//    public function newGameHasADealer()
-//    {
-//        $this->assertInstanceOf(Dealer::class, $this->game->dealer);
-//    }
-//
-//    /**
-//     * @test
-//     */
-//    public function RoundWinnerIsSaved()
-//    {
-//
-//    }
-//}
+class GameTest extends PHPUnit_Framework_TestCase
+{
+
+    public $game;
+
+    public function setUp()
+    {
+        ob_start();
+        $this->game = new Game();
+        ob_end_clean();
+    }
+
+    /**
+     * @test
+     */
+    public function canCreateAGame()
+    {
+        $this->assertInstanceOf(Game::class, $this->game);
+    }
+
+    /**
+     * @test
+     */
+    public function newGameHasTwoBots()
+    {
+        $this->assertInstanceOf(Bot::class, $this->game->botOne);
+        $this->assertInstanceOf(Bot::class, $this->game->botTwo);
+    }
+
+    /**
+     * @test
+     */
+    public function newGameHasADealer()
+    {
+        $this->assertInstanceOf(Dealer::class, $this->game->dealer);
+    }
+}
