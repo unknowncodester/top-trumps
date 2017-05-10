12f8f14af772735e0cede71ad8bee4355a36c301
Scott Dutton <scott@exussum.co.uk>
2017-05-10 20:34:53 +0100

    Add a test helper for covering stdin

--- tests/PlayableTest.php
+++ tests/PlayableTest.php
@@ -2,58 +2,41 @@
 
 class PlayableTest extends PHPUnit_Framework_TestCase
 {
-    protected $playablePlayer;
+
+    protected $player;
 
     public function setUp()
     {
-        $this->playablePlayer = new Playable('Player 1');
+        $this->player = new Playable("John McClain");
+        ob_start();
+    }
+
+    public function tearDown()
+    {
+        ob_end_clean();
     }
 
     /**
      * @test
+     * @dataProvider readLocations
      */
-    public function canBeCreated()
+    public function canReadFromFile($expected, $location)
     {
-        $this->assertInstanceOf(Playable::class, $this->playablePlayer);
+        $this->player->setReadLocation(__DIR__ . "/Fixtures/firstChoice.txt");
+        $this->assertSame("Rage", $this->player->takeTurn());
     }
 
-    /**
-     * @test
-     */
-    public function canGetName()
+    public function readLocations()
     {
-        $this->assertEquals('Player 1', $this->playablePlayer->getName());
-    }
-
-    /**
-     * @test
-     */
-    public function canCollectACard()
-    {
-        $initialDeckSize = count($this->playablePlayer->cardDeck);
-        $this->playablePlayer->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
-        $this->assertEquals($initialDeckSize + 1, count($this->playablePlayer->cardDeck));
-    }
-
-    /**
-     * @test
-     */
-    public function canLoseACard()
-    {
-        $this->playablePlayer->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
-        $initialDeckSize = count($this->playablePlayer->cardDeck);
-        $this->playablePlayer->getCard();
-        $this->assertEquals($initialDeckSize - 1, count($this->playablePlayer->cardDeck));
-    }
-
-    /**
-     * @test
-     */
-    public function cardLostIsACard()
-    {
-        $this->playablePlayer->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
-        $card = $this->playablePlayer->getCard();
-        $this->assertInstanceOf(Card::class, $card);
+        return [
+            [
+                'Rage',
+                __DIR__ . "/Fixtures/firstChoice.txt"
+            ],
+            [
+                'Rage',
+                __DIR__ . "/Fixtures/secondChoice.txt"
+            ],
+        ];
     }
 }
-?>
