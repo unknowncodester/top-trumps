0b2a9e2db240eb23a81732faf6740d7fa0eb80ff
Scott Dutton <scott@exussum.co.uk>
2017-05-10 20:41:58 +0100

    Bring the lost tests back

--- tests/PlayableTest.php
+++ tests/PlayableTest.php
@@ -39,4 +39,43 @@ class PlayableTest extends PHPUnit_Framework_TestCase
             ],
         ];
     }
+
+    /**
+     * @test
+     */
+    public function canGetName()
+    {
+        $this->assertEquals('John McClain', $this->player->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function canCollectACard()
+    {
+        $initialDeckSize = count($this->player->cardDeck);
+        $this->player->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
+        $this->assertEquals($initialDeckSize + 1, count($this->player->cardDeck));
+    }
+
+    /**
+     * @test
+     */
+    public function canLoseACard()
+    {
+        $this->player->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
+        $initialDeckSize = count($this->player->cardDeck);
+        $this->player->getCard();
+        $this->assertEquals($initialDeckSize - 1, count($this->player->cardDeck));
+    }
+
+    /**
+     * @test
+     */
+    public function cardLostIsACard()
+    {
+        $this->player->collectCard(new Card("Lab rat", 10, 6, 5, 3, 5, 4));
+        $card = $this->player->getCard();
+        $this->assertInstanceOf(Card::class, $card);
+    }
 }
